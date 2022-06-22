e864a80d55c808bb60797feee3afec8d2168b2ff
mustafaalameen <Mustafa Al-ameen Mofeyisayo>
2022-06-23 00:25:08 +0100

    First javascript task

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('#name').textContent = 'Mustafa Umar Omotayo'
+document.querySelector('#height').textContent = '5ft'
+document.querySelector('#country').textContent = 'Nigeria'
